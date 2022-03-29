373c9fcec7f52a70e30fe6f909fdcd35cb347785
Appazov <anvar978@yandex.ru>
2022-03-29 20:29:22 +0400

    Add error handling

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const sequelize = require('./db/db')
 const models = require('./models/models')
 const cors = require('cors')
 const router = require('./routes/index')
+const errorHandler = require('./middlewares/ErrorHandlingMiddleware')
 
 const PORT = process.env.PORT || 5000
 
@@ -11,6 +12,9 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/api', router)
+
+// last error handling middleware
+app.use(errorHandler)
 
 const start = async () => {
   try {
