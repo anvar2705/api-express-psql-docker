0b415099de1e1aaf455e3543b6d8e842c0b5f24c
Appazov <anvar978@yandex.ru>
2022-04-05 13:23:14 +0400

    Add users, countries, genres mocks to database

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db/db')
-const models = require('./models/models')
 const cors = require('cors')
 const router = require('./routes/index')
 const errorHandler = require('./middlewares/errorHandlingMiddleware')
+const createUserMock = require('./utils/createUserMock')
+const createGenreMock = require('./utils/createGenreMock')
+const createCountryMock = require('./utils/createCountryMock')
 
 const PORT = process.env.PORT || 5000
 
@@ -20,6 +22,30 @@ const start = async () => {
   try {
     await sequelize.authenticate()
     await sequelize.sync({ alter: true })
+
+    // create default users
+    createUserMock('admin', 'admin123', 'ADMIN')
+    createUserMock('user', 'user123', 'USER')
+    createUserMock('Anvar', 'anvar123', 'USER')
+
+    //create default genres
+    createGenreMock('Комедия')
+    createGenreMock('Ужасы')
+    createGenreMock('Мультфильм')
+    createGenreMock('Фантастика')
+    createGenreMock('Драма')
+    createGenreMock('Исторический')
+    createGenreMock('Документальный')
+    createGenreMock('Детский')
+    createGenreMock('Боевик')
+    createGenreMock('Криминал')
+    createGenreMock('Детектив')
+
+    //create default countries
+    createCountryMock('Россия')
+    createCountryMock('США')
+    createCountryMock('Китай')
+    createCountryMock('Великобритания')
 
     app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
   } catch (e) {
